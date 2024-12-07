88a799ee4871d4c7f121063952f05d52b8a95b95
Salai Chit Oo Latt <salaichitoolatt@outlook.com>
2024-12-08 00:18:44 +0630

    1

--- PoolPuzzleOne.java
+++ PoolPuzzleOne.java
@@ -1,21 +1,23 @@
-ass PoolPuzzleOne {
+class PoolPuzzleOne {
   public static void main(String[] args) {
     int x = 0;
     while (x < 4) {
-      //
+      x = x + 2;
       if (x < 1) {
-        // 
+        System.out.print("a");
+	System.out.print(" ");
+	System.out.print("noise");
       }
-      //
-      if () {
-        //
-	//
+      x = x - 1;
+      if (x > 1) {
+        System.out.print("an");
+	System.out.print("noys");
       }
       if (x == 1) {
-        //
+        System.out.print("an");
       }
-      if () {
-        //
+      if (x > 3) {
+        System.out.print(" oyster");
       }
       System.out.println();
       x = x + 1;
