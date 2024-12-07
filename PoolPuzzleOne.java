3df1f6ccfd529a9bf7c19b7a65a45fd710806764
Salai Chit Oo Latt <salaichitoolatt@outlook.com>
2024-12-07 23:38:57 +0630

    PoolPuzzleOne.java added

---
+++ PoolPuzzleOne.java
@@ -0,0 +1,24 @@
+ass PoolPuzzleOne {
+  public static void main(String[] args) {
+    int x = 0;
+    while (x < 4) {
+      //
+      if (x < 1) {
+        // 
+      }
+      //
+      if () {
+        //
+	//
+      }
+      if (x == 1) {
+        //
+      }
+      if () {
+        //
+      }
+      System.out.println();
+      x = x + 1;
+    }
+  }
+}
